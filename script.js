abb1afe75c3fddb7f0f882680edde407210419d5
ioloEJ42 <ioloevans42@gmail.com>
2024-02-09 11:26:22 +0000

    Inserted Modal, changed theme, added info

--- script.js
+++ script.js
@@ -38,3 +38,35 @@ document.getElementById("shift").addEventListener("input", handleCipher);
 
 // Initialize the cipher with the default form values on page load
 window.addEventListener("load", handleCipher);
+
+// Popup Code----------------------------------------------------------------
+// Get the modal
+var modal = document.getElementById("infoModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("infoButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
+
+btn.onclick = function() {
+    console.log("Button clicked");
+    modal.style.display = 'block';
+};
